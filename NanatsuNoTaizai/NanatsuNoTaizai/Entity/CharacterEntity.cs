431a6e672b1bc371ce95982db7e52d12725dadda
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-10-29 05:51:46 +0300

    Create Entity

---
+++ NanatsuNoTaizai/NanatsuNoTaizai/Entity/CharacterEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanatsuNoTaizai.Entity
+{
+    public class CharacterEntity
+    {
+        public short CharacterID { get; set; }
+        public string CharacterName { get; set; }
+        public short CharacterAge { get; set; }
+        public int CharacterAbilities { get; set; }
+    }
+}
---
+++ NanatsuNoTaizai/NanatsuNoTaizai/Entity/Context.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace NanatsuNoTaizai.Entity
+{
+    public class Context
+    {
+        public DbSet<CharacterEntity> characterEntities { get; set; }
+    }
+}
